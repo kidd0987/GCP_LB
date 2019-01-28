d87b83742a7e8d666603423fdaad5977a66d4c83
kidd0987 <kidd0987@gmail.com>
2019-01-28 21:38:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+from gevent.wsgi import WSGIServer
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -38,5 +39,7 @@ def handle_message(event):
 
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
